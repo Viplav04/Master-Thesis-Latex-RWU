cd9c2ed6ff14edbd39cf5142dba001d0512285a6
Viplav Setia <55150569+Viplav04@users.noreply.github.com>
2019-12-09 13:41:15 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+
+cd examples
+
+rm thesis.nlo thesis.ist thesis.ilg thesis.blg thesis.bbl
+
+cd ..
+
+make
+
+makeindex examples/thesis.nlo -s nomencl.ist -o thesis.nls
+
+chmod +x thesis.nls
+mv thesis.nls examples
+
+bibtex examples/thesis
+mv thesis.bib examples
+
+echo "moved"
+
+make
+
